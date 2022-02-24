8f47af5b5fbac85005e444dbc259156113daaf44
ivopozzani <ivo.pozzani@gmail.com>
2022-02-24 10:41:04 -0300

    feat: added spec tests

--- spec/services/get_user_service_spec.rb
+++ spec/services/get_user_service_spec.rb
@@ -3,25 +3,40 @@ require 'rails_helper'
 RSpec.describe GetUserService, type: :controller do
   describe '#call' do
     subject(:service) { GetUserService.call(cart, params) }
-    let(:params) do { email: 'user@spec.io', first_name: "John", last_name: "Doe" } end
+    let(:params) { { email: 'user@user.io', first_name: 'Userfromparams', last_name: 'Doe' } }
 
-    context 'when user is created' do
-      let(:cart) { create(:cart) }
-      
-      it 'returns a not guest user' do
-      service
-    
-      expect(User.first).not_to be_guest
+    context 'when user exists' do
+      let(:user) { create(:user) }
+      let(:cart) { create(:cart, user_id: user.id) }
+
+      it 'returns user' do
+        expect(service).to eq(user)
+      end
+
+      it 'does not return guest user' do
+        service
+
+        expect(user).not_to be_guest
       end
     end
 
-    context 'when user is not created' do
+    context 'when user does not exist' do
       let(:cart) { create(:cart, user_id: nil) }
-     
-      it 'returns a guest user' do
-      service
 
-      expect(User.first).to be_guest
+      it 'creates user' do
+        expect { service }.to change(User, :count).by(1)
+      end
+
+      it 'returns user using attributes from params' do
+        service
+
+        expect(User.last.first_name).to eq('Userfromparams')
+      end
+
+      it 'returns guest user' do
+        service
+
+        expect(User.last).to be_guest
       end
     end
   end
--- spec/services/process_order_service_spec.rb
+++ spec/services/process_order_service_spec.rb
@@ -2,16 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ProcessOrderService, type: :controller do
   describe '#call' do
-    let (:cart) { create(:cart) }
-    let (:user) { cart.user }
-    let (:params) { nil }
+    let(:cart) { create(:cart) }
+    let(:user) { cart.user }
+    let(:params) do
+      { address_1: 'add1', address_2: 'add2', city: 'city1', state: 'state1', country: 'country1', zip: '44444' }
+    end
 
     it 'creates new order' do
       expect { ProcessOrderService.call(cart, user, params) }.to change(Order, :count).by(1)
     end
 
     it 'returns order object' do
-      expect(ProcessOrderService.call(cart, user, params)).to eq(Order.first)
+      expect(ProcessOrderService.call(cart, user, params)).to eq(Order.last)
     end
   end
 end
